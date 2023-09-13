dd1129b4eeafb58149fdb39183fab829a2498a8a
sophia abdelrahman <sophiasabdelrahman@gmail.com>
2023-09-13 12:04:35 -0400

    shrink sample log size

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os
 LOG_DIR = os.path.join(os.getcwd())
 LOG_FILE_NAME = 'sample.log'
 LOG_FILE_PATH = os.path.join(LOG_DIR, LOG_FILE_NAME)
-LOG_FILE_SIZE_GB = 0.001
+LOG_FILE_SIZE_GB = 0.00001
 
 def configure_logger():
     logging.basicConfig(level=logging.DEBUG)
